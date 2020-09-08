18e1b6f190883361cab49a7748b5f2e17765e6d4
MilkyasEyob.Habte <milkyas23>
2020-09-08 13:41:01 +0200

    fungerande Funktion

--- src/circle.java
+++ src/circle.java
@@ -6,8 +6,14 @@ public class circle {
 
     public circle() {
         radius = 4.0;
-
-
-
+    }
+    public circle(double radius) {
+        this.radius = radius;
+    }
+    public double getArea() {
+        return radius*radius*Math.PI;
+    }
+    public double getCircumference(){
+    return  radius*2*Math.PI;
     }
 }
---
+++ src/circleTest.java
@@ -0,0 +1,7 @@
+public class circleTest {
+    public static void main(String[] args) {
+        circle c = new circle();
+        System.out.println(c.getArea());
+        System.out.println(c.getCircumference());
+    }
+}
